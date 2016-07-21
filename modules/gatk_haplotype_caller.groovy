ceef91553591051e0344f863de81e353c8f0651d
Marc <m.hoeppner@ikmb.uni-kiel.de>
2016-07-21 15:54:53 +0200

    Refactoring GATK module

--- modules/gatk_haplotype_caller.groovy
+++ modules/gatk_haplotype_caller.groovy
@@ -1,12 +1,12 @@
 gatk_haplotype_caller = {
 
-        doc about: "Call haplotypes from BAM file",
-        description: "Compute haplotypes from BAM file using GATK",
+        doc about: "Call haplotypes from BAM files",
+        description: "Compute haplotypes from a list of BAM files using GATK",
         constraints: "Must have GATK in PATH",
         author: "mphoeppner@gmail.com"
 
         // Variables here
-        var procs : 1          // Number of cores to use
+        var procs : 16          // Number of cores to use
         var directory : ""      // Allows specifying an output directory
         var memory : "22"
 
@@ -16,8 +16,31 @@ gatk_haplotype_caller = {
         requires SNP_REF : "Must provide reference SNPS for GATK"
         requires DBSNP_REF : "Must provide dbSNP reference"
 
-	exec """
-		 java -XX:ParallelGCThreads=1 -jar -Xmx${memory}g $GATK
-                 -T BaseRecalibrator
-	"""
+	def vcf_file = branch.name + ".gatk.raw.vcf"
+
+	produce(vcf_file) {
+		exec """
+			java -XX:ParallelGCThreads=1 -jar -Xmx${memory}g $GATK
+                	-T BaseRecalibrator
+			-nct $procs
+			-minPruning 4 -minReadsPerAlignStart 10
+			-R $REF
+			-I $input
+			--dbsnp $DBSNP_REF
+			-stand_call_conf 50.0
+			-stand_emit_conf 10.0
+			-mbq 10
+			-TARGET_FILE $TARGET_FILE
+			-L chrM
+			-o $output
+		"""
+	}
+
+	 // Validation here?
+
+        check {
+                exec "[ -s $output ]"
+        } otherwise {
+                fail "Output empty, terminating $branch.name"
+        }
 }
---
+++ modules/gatk_select_variants.groovy
@@ -0,0 +1,46 @@
+gatk_select_variants = {
+
+        doc about: "Select variants from vcf file",
+        description: "Run the SelectVariants method from GATK",
+        constraints: "Must have GATK in PATH",
+        author: "mphoeppner@gmail.com"
+
+        // Variables here
+        var procs : 1          // Number of cores to use
+        var directory : ""      // Allows specifying an output directory
+        var memory : "22"
+	var select : "SNP"
+
+        // Requires
+        requires GATK : "Must provide path to GATK"
+        requires REF  : "Must provide reference file for GATK"
+
+	def extension = ""
+
+	if (select == "SNP") {
+		extension = "snps"
+	} else if (select == "INDEL") {
+		extension = "indels"
+	} else {
+		fail "SelectType (select) must be either SNP or INDEL"
+	}
+
+	filter(extension) {
+		 exec """
+        	                java -XX:ParallelGCThreads=1 -jar -Xmx${memory}g $GATK
+                	                -T SelectVariants
+					-selectType $select
+                        	        -V $input
+	                                -o $output
+        	                        -R $REF
+	                ""","gatk_select_variants"
+	}
+
+        // Validation here?
+
+        check {
+                exec "[ -s $output ]"
+        } otherwise {
+                fail "Output empty, terminating $branch.name"
+        }
+}
